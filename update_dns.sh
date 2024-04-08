eda41fb6f887a671b4e8197078b8ccc3037f0c36
sakkoulas <sakkoulas@gmail.com>
2024-04-08 07:52:13 +0300

    read credentails problem

--- update_dns.sh
+++ update_dns.sh
@@ -4,9 +4,14 @@
 IP=$(curl -s http://ipv4.icanhazip.com)
 
 # Load Cloudflare credentials
-source cloudflare_credentials.txt
-
  
+#  For Debian 
+. ./cloudflare_credentials.txt
+
+# For Centos and others
+# source ./cloudflare_credentials.txt
+ 
+echo $IP 
 # start loop to update all subdomains
 while read -r SUBDOMAIN; do
     RECORD_NAME="$SUBDOMAIN.$DOMAIN"
@@ -15,7 +20,8 @@ while read -r SUBDOMAIN; do
          -H "X-Auth-Email: $EMAIL" \
          -H "Authorization: Bearer $API_KEY" \
          -H "Content-Type: application/json" | jq -r '.result[0].id')
-     
+echo $SUBDOMAIN
+ 
     # Ενημέρωση του DNS record
     RESPONSE=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID" \
          -H "X-Auth-Email: $EMAIL" \
